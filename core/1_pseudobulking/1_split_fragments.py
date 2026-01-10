fixed tss enrichment calculation to match snapatac2 conventions

core/1_pseudobulking/1_split_fragments.py
@@ -32,8 +32,10 @@ def process_fragments_file(fragments_file_name, data_dir, metadata_loc, at_annot
     # Get allowed chromosomes
     chr_sizes_df = pd.read_csv(chr_sizes_loc, sep="\t", names=["chr", "size"])
     allowed_chrs = set(chr_sizes_df["chr"].unique().tolist())
-    # Get TSS locations
+    # Get TSS locations/data
     tss_by_chr_np = load_tss_locs(tss_locs_loc)
+    TSS_half_window = 2000
+    TSS_half_smooth_window = 5
     # Set up outfiles
     output_file_handles = dict()
     output_file_handles["pseudorep1"] = dict()
@@ -47,7 +49,7 @@ def process_fragments_file(fragments_file_name, data_dir, metadata_loc, at_annot
         output_file_handles["fragments"][p] = open(f"{data_dir}/separated_fragments/{p}-{fragments_file_name}.tsv", 'w')
     # Iterate through fragments file
     fragments_file_loc = f"{data_dir}/raw_fragments/{fragments_file_name}.bed.gz"
-    cell_qc = defaultdict(lambda: {"annotated": False, "num_unique_frags": 0, "num_reads": 0, "num_dup_reads": 0, "mono_nucleosomal_frags": 0, "nucleosome_free_frags": 0, "tss_insertions": np.zeros((2001, ))})
+    cell_qc = defaultdict(lambda: {"annotated": False, "num_unique_frags": 0, "num_reads": 0, "num_dup_reads": 0, "mono_nucleosomal_frags": 0, "nucleosome_free_frags": 0, "tss_insertions": np.zeros((2*TSS_half_window+1, ))})
     num_lines = 0
     start_time = time.time()
     with gzip.open(fragments_file_loc, 'rt') as f:
@@ -73,10 +75,14 @@ def process_fragments_file(fragments_file_name, data_dir, metadata_loc, at_annot
             if (chro in tss_by_chr_np) and (chro not in ["chrM"]):
                start_tss_pos = check_tss_overlap(start_shifted, tss_by_chr_np[chro][0], tss_by_chr_np[chro][1])
                if start_tss_pos is not None:
-                   barcode_qc["tss_insertions"][start_tss_pos] += 1
+                   for p in start_tss_pos:
+                       # p is between -L and L
+                       barcode_qc["tss_insertions"][p+TSS_half_window] += 1
                end_tss_pos = check_tss_overlap(end_shifted-1, tss_by_chr_np[chro][0], tss_by_chr_np[chro][1])
                if end_tss_pos is not None:
-                   barcode_qc["tss_insertions"][end_tss_pos] += 1
+                   for p in end_tss_pos:
+                       # p is between -L and L
+                       barcode_qc["tss_insertions"][p+TSS_half_window] += 1
             # Remove nonstandard chromosomes
             if chro not in allowed_chrs:
                 continue
@@ -127,8 +133,8 @@ def process_fragments_file(fragments_file_name, data_dir, metadata_loc, at_annot
         qc_row["nucleosomal_signal"] = (1+barcode_qc["mono_nucleosomal_frags"])/(1+barcode_qc["nucleosome_free_frags"])
         tss_insertions = barcode_qc["tss_insertions"]
         tss_insertions_flank_mean = (np.sum(tss_insertions[:100]) + np.sum(tss_insertions[-100:]))/200
-        tss_insertions_center = np.mean(tss_insertions[1000-2:1000+3])
-        qc_row["tss_enrichment"] = (tss_insertions_center/(tss_insertions_flank_mean if tss_insertions_flank_mean > 0 else 1))
+        tss_insertions_center = np.mean(tss_insertions[TSS_half_window-TSS_half_smooth_window:TSS_half_window+TSS_half_smooth_window+1])
+        qc_row["tss_enrichment"] = tss_insertions_center/(tss_insertions_flank_mean + 0.1) # add 0.1 like snapatac2 to avoid division by zero
         # Raw values
         qc_row["raw-num_reads"] = barcode_qc["num_reads"]
         qc_row["raw-num_dup_reads"] = barcode_qc["num_dup_reads"]

core/1_pseudobulking/5_aggregate_outputs.py
@@ -94,10 +94,13 @@ def aggregate_files(data_dir, metadata_loc, at_annotation_level):
         pseudobulk_qc_summary["num_frags"] = pseudobulk_atac_qc_raw["num_frags"].sum()
         pseudobulk_qc_summary["pct_duplicated_reads"] = (pseudobulk_atac_qc_raw["raw-num_dup_reads"].sum() / pseudobulk_atac_qc_raw["raw-num_reads"].sum()) * 100
         pseudobulk_qc_summary["nucleosomal_signal"] = (1 + pseudobulk_atac_qc_raw["raw-mono_nucleosomal_frags"].sum()) / (1 + pseudobulk_atac_qc_raw["raw-nucleosome_free_frags"].sum())
+        # ATAC - TSS enrichment
+        TSS_half_window = 2000
+        TSS_half_smooth_window = 5
         pseudobulk_tss_insertions = pseudobulk_tss_matrix.sum(axis=0)
         tss_insertions_flank_mean = (np.sum(pseudobulk_tss_insertions[:100]) + np.sum(pseudobulk_tss_insertions[-100:])) / 200
-        tss_insertions_center = np.mean(pseudobulk_tss_insertions[1000-2:1000+3])
-        pseudobulk_qc_summary["tss_enrichment"] = (tss_insertions_center / (tss_insertions_flank_mean if tss_insertions_flank_mean > 0 else 1))
+        tss_insertions_center = np.mean(pseudobulk_tss_insertions[TSS_half_window-TSS_half_smooth_window:TSS_half_window+TSS_half_smooth_window+1])
+        pseudobulk_qc_summary["tss_enrichment"] = (tss_insertions_center / (tss_insertions_flank_mean + 0.1)) # add 0.1 like snapatac2 to avoid division by zero
         # ATAC - FRIP
         fragments_per_cell_df = pd.read_csv(f"{data_dir}/peaks/{pseudobulk}-fragments_per_cell.txt", sep=" ", names=["barcode", "num_fragments"])
         fragments_in_peaks_per_cell_df = pd.read_csv(f"{data_dir}/peaks/{pseudobulk}-fragments_in_peaks_per_cell.txt", sep=" ", names=["barcode", "num_fragments_in_peaks"])

core/1_pseudobulking/utils.py
@@ -30,25 +30,22 @@ def load_tss_locs(tss_locs_loc):
     tss_by_chr_np = dict()
     for chro in tss_locs_df["chro"].unique():
         chro_tss_df = tss_locs_df[tss_locs_df["chro"] == chro]
-        tss_positions = chro_tss_df["TSS"].values
+        tss_positions = chro_tss_df["TSS"].values - 1 # GTF is 1-based but fragments are 0-based
         strand_signs = np.array([1 if s == "+" else -1 for s in chro_tss_df["strand"].values], dtype=int)
-        sorted_indices = np.argsort(tss_positions)
-        tss_by_chr_np[chro] = (tss_positions[sorted_indices], strand_signs[sorted_indices])
+        df = pd.DataFrame()
+        df["TSS"] = tss_positions
+        df["strand"] = strand_signs
+        df.sort_values(by=["TSS", "strand"], inplace=True)
+        df = df.drop_duplicates()
+        tss_by_chr_np[chro] = (df["TSS"].to_numpy(), df["strand"].to_numpy())
     return tss_by_chr_np
 
 
 @njit
 def check_tss_overlap(position, tss_vec, strand_vec):
-    i = np.searchsorted(tss_vec, position)
-    if i == 0:
-        idxs = [i]
-    elif i == len(tss_vec):
-        idxs = [i-1]
-    else:
-        idxs = [i-1, i]
-    idxs = np.array(idxs)
-    distances = (position - tss_vec[idxs]) * strand_vec[idxs]
-    abs_distances = np.abs(distances)
-    if np.any(abs_distances <= 1000):
-        return distances[np.argmin(abs_distances)] + 1000
-    return None
+    L = 2000 # half window size
+    tss_left_idx = np.searchsorted(tss_vec, position-L, side='left')
+    tss_right_idx = np.searchsorted(tss_vec, position+L, side='right')-1
+    if tss_left_idx > tss_right_idx:
+        return None
+    return [(position - tss_vec[i]) * strand_vec[i] for i in range(tss_left_idx, tss_right_idx+1)]