825bf093d39f020b7cef648c9ab4e70af465bd15
Salil Deshpande <salil512@stanford.edu>
2026-05-15 01:01:02 +0000

    remove at_annotation_level extra reference

--- core/1_pseudobulking/1_split_fragments.sh
+++ core/1_pseudobulking/1_split_fragments.sh
@@ -3,17 +3,16 @@ set -euo pipefail
 
 datadir="${1}"
 metadata_loc="${2}"
-at_annotation_level="${3}"
-chr_order_file="${4}"
-tss_file="${5}"
-parallel="${6}"
+chr_order_file="${3}"
+tss_file="${4}"
+parallel="${5}"
 
 raw_frags_dir="${datadir}/raw_fragments"
 
 # Get current script dir
 scriptdir="$(dirname "$(realpath $0)")"
 
-ls ${raw_frags_dir}/*.bed.gz | grep -oP '(?<=/)\w+(?=\.)' | xargs -I {} -P ${parallel} python ${scriptdir}/1_split_fragments.py -d ${datadir} -f {} -m "${metadata_loc}" -a "${at_annotation_level}" -c "${chr_order_file}" -t "${tss_file}"
+ls ${raw_frags_dir}/*.bed.gz | grep -oP '(?<=/)\w+(?=\.)' | xargs -I {} -P ${parallel} python ${scriptdir}/1_split_fragments.py -d ${datadir} -f {} -m "${metadata_loc}" -c "${chr_order_file}" -t "${tss_file}"
 
 # Validate: every raw fragment file should have a corresponding QC report
 failed=0
